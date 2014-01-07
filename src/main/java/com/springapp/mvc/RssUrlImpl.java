43873c9e46b6fcbf2af87706b4ac0dbee9fc461d
Heikki Juntunen <heikki.juntunen@elisa.fi>
2014-01-07 17:48:50 +0200

    Use real rss-feeds

--- src/main/java/com/springapp/mvc/RssUrlImpl.java
+++ src/main/java/com/springapp/mvc/RssUrlImpl.java
@@ -3,7 +3,6 @@ package com.springapp.mvc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -17,8 +16,10 @@ public class RssUrlImpl implements RssUrl {
     public List<URL> get() {
         List<URL> urls = new ArrayList<>();
         try {
-            urls.add(new File("feed1.xhtml").toURI().toURL());
-            urls.add(new File("feed2.xhtml").toURI().toURL());
+            urls.add(new URL("http://rss.kauppalehti.fi/rss/etusivun_uutiset.jsp"));
+            urls.add(new URL("http://rss.kauppalehti.fi/rss/yritysuutiset.jsp"));
+            urls.add(new URL("http://rss.kauppalehti.fi/rss/omaraha.jsp"));
+            urls.add(new URL("http://rss.kauppalehti.fi/rss/auto.jsp"));
         } catch (MalformedURLException e) {
             logger.error("invalid url", e);
         }
